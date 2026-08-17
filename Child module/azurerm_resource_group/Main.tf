5319b653a95708562c4d57e4bab5765e001dfaee
Ankit Bansal <ankitbansal.6aug@gmail.com>
2026-08-17 18:59:25 +0530

    updated

--- Child module/azurerm_resource_group/Main.tf
+++ Child module/azurerm_resource_group/Main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "config" {
-  for_each = var.config
+  for_each = var.rgs
   name     = each.value.name
   location = each.value.location
 }
--- Parent module/Main.tf
+++ Parent module/Main.tf
@@ -1,4 +1,4 @@
 module "resource_group" {
   source = "../Child module/azurerm_resource_group"
-  config = var.rgs
+  rgs = var.rgs
 }
